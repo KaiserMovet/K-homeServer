362a75d9cab83e5314a389a3a634d1e390a68e61
KaiserMovet <rynkiewiczmate@gmail.com>
2020-06-07 23:23:11 +0200

    use moment to print current date

--- app/static/ic_site/script.js
+++ app/static/ic_site/script.js
@@ -1,17 +1,7 @@
 function startTime() {
-
-    var today = new Date();
-    var Y = today.getFullYear();
-    var m = checkTime(today.getMonth());
-    var d = checkTime(today.getDay());
-    var H = today.getHours();
-    var M = checkTime(today.getMinutes());
-    var S = checkTime(today.getSeconds());
-
     document.getElementById('time').innerHTML =
-        Y + "." + m + "." + d + " " + H + ":" + M + ":" + S;
-
-    var t = setTimeout(startTime, 500);
+        moment().format('YYYY.MM.DD HH:mm:ss');
+    var t = setTimeout(startTime, 1000);
 }
 function checkTime(i) {
     if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
